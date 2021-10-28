d609a2528a80e82e0a3acab7c83dcf8d4791b45d
sgcm14 <sgcm14@gmail.com>
2021-10-28 12:55:31 -0500

    add config. firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'lab-notes-fdc81',
+    appId: '1:481348563835:web:fc71a7f4950fde4c6603de',
+    storageBucket: 'lab-notes-fdc81.appspot.com',
+    apiKey: 'AIzaSyB9_QaIFnK-XMW08z1X_Xl3gP47P3HlWFk',
+    authDomain: 'lab-notes-fdc81.firebaseapp.com',
+    messagingSenderId: '481348563835',
+  },
   production: true
 };
