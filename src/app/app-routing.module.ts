ff6aee956cf8ed45acb4facf070149ed17a78599
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2023-01-04 18:03:51 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
+import { RegisterWithPhoneComponent } from './components/register-with-phone/register-with-phone.component';
 
 
 const routes: Routes = [
-  {path : '', redirectTo: 'login', pathMatch: 'full'},
-  {path : 'login', component:LoginComponent},
-  {path : 'register', component:RegisterComponent},
-  {path : 'home', component:HomeComponent},
-  {path : '**', redirectTo: 'login', pathMatch: 'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'registerPhone', component: RegisterWithPhoneComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
